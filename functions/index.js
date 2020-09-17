f775263a9c20f40755f19102da1ade678a74d562
simran1997 <simranjeet6344@gmail.com>
2020-09-17 01:42:41 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 //secret key from stripe
-const stripe = require("stripe")("");
+const stripe = require("stripe")("");     //add your secret key here
 
 // API
 
